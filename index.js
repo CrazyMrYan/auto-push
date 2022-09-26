ccd1d435e929df3842f37e550fd3bbf231893f2c
严家辉 <yanjiahui@192.168.0.106>
2022-09-27 00:13:01 +0800

    init: auto script

---
+++ index.js
@@ -0,0 +1,55 @@
+const schedule = require('node-schedule');
+const child_process = require('child_process');
+const fs = require('fs');
+const FILE_PATH = `${__dirname}/README.md`
+const moment = require('moment');
+const newDate = () => moment().format('YYYY-MM-DD HH:mm:ss');
+
+
+schedule.scheduleJob('30 * * * * *', async () => {
+    await editFile();
+    implementShell('git add .', console.log)
+    implementShell('git commit -m "feat: 自动推送"', console.log)
+    implementShell('git push -u origin master', console.log)
+
+});
+
+const implementShell = async (shell, callback) => {
+    callback(`[${newDate()}] ${shell}`)
+    return new Promise((resolve, reject) => {
+        try {
+            const sh = child_process.exec(`cd ${__dirname} && ${shell}`, (error, stdout, stderr) => {
+                if (error) {
+                    reject(error);
+                    callback(`[${newDate()}] ${error}`)
+                }
+                resolve()
+            });
+            sh.stdout.on('data', (data) => {
+                callback(`[${newDate()}] ${data}`)
+            })
+            sh.stderr.on('data', (error) => {
+                callback(`[${newDate()}] ${error}`)
+            })
+        } catch (error) {
+            callback(`[${newDate()}] ${error}`)
+            reject(error)
+        }
+    })
+}
+
+const editFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(FILE_PATH, 'utf-8', (err, data) => {
+            if (err) {
+                reject(err);
+                throw err;
+            }
+            const temp = `${data}
+- ${newDate()}
+`
+            fs.writeFile(FILE_PATH, temp, null, err => err ? reject(err) : resolve())
+        });
+    })
+}
+
